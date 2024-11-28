e48ef4b12e51f73546bccb7c437dc57e4d969ecb
phamtuan301 <tuan13nt@gmail.com>
2024-11-28 20:46:01 +0700

    Add files via upload

---
+++ Session8-ex4.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	int arr[3][3]={1,2,3,4,5,6,7,8,9};
+	int max = arr[0][0];
+	for (int i=0;i<3;i++){
+		for(int j=0;j<3;j++){
+			if(arr[i][j]>max){
+				max = arr[i][j];
+			}
+		}
+	}
+	printf("phan tu lon nhat cua mang la: %d", max);
+	return 0;
+}
